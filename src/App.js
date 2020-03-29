50cdfaa7c05430959debdb3a7937259209651e0c
Adhithyakrishna <akovaisr@buffalo.edu>
2020-03-29 02:34:07 -0400

    Creating the boiler plate code from scratch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* code for root component of our applications*/
+
+import React from 'react';
+
+import './App.css';
+
+const App = () =>(
+    <div className ="App">
+        <h1>Hello world</h1>    
+    </div>
+)
+
+export default App;
